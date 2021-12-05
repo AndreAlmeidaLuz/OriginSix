4743e58fd285b30a2acaf4ad3b16df88cd2e743b
Andre Almeida <almeidaandreluz@gmail.com>
2021-12-05 14:52:39 -0300

    Atualizacao

--- script.js
+++ script.js
@@ -21,24 +21,33 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    //scroll é maior que a altura do header
-    header.classList.add('scroll')
-  } else {
-    //menor que a altura do header
-    header.classList.remove('scroll')
-  }
-})
+function changeHeaderWhenScroll() {
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= navHeight) {
+      //scroll é maior que a altura do header
+      header.classList.add('scroll')
+    } else {
+      //menor que a altura do header
+      header.classList.remove('scroll')
+    }
+  })
+}
 
 /*tESTIMONIUS CARROUSEL SWIPER*/
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    //dimensao da tela
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /*====== SCROLL REVEAL mostrar elementos quando der sroll na pagina =======*/
@@ -53,7 +62,49 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/*Botao voltar para o topo*/
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*ativar menu conforme a sessao visivel na pagiina */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
